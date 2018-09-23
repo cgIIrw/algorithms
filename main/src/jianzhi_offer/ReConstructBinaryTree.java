375ad76e5a0cdbfc5f7fadef3fb9eb808f810729
cgIIrw <chenguo_jay@163.com>
2018-09-23 18:27:07 +0800

    马奎斯

--- main/src/jianzhi_offer/ReConstructBinaryTree.java
+++ main/src/jianzhi_offer/ReConstructBinaryTree.java
@@ -3,15 +3,52 @@ package jianzhi_offer;
 import jianzhi_offer.tree_struct.BinaryTreeNode;
 
 /**
- * 重建二叉树
- * 题目描述：
- * 根据前序遍历和中序遍历遍历的值，重建二叉树，假设前序中序不含重复数
+ * 马奎斯 重建二叉树
  */
 public class ReConstructBinaryTree {
 
-//    public static BinaryTreeNode reConstructBinaryTree(int[] pre, int[] in) {
-//
-//
-//        return
+    public static BinaryTreeNode reConstructBinaryTree(int[] l1, int head01, int tail01,
+                                                       int[] l2, int head02, int tail02) {
+        if (l1 == null || l2 == null || l1.length != l2.length || l1.length <= 0) {
+            return null;
+        }
+
+        // 这里约定当head < 0 || tail > l.length - 1的时候数组为空；
+        if (head01 < 0 || tail01 > l1.length - 1
+                || head02 < 0 || tail02 > l2.length - 1
+                || head01 > tail01 || head02 > tail02) {
+            return null;
+        }
+
+        int temp = 0;
+        for (int i = head02; i <= tail02; i++) {
+            if (l1[head01] == l2[i]) {
+                temp = i;
+                break;
+            } else if (i == tail01) {
+                return null;
+            }
+        }
+
+        BinaryTreeNode root = new BinaryTreeNode(l1[head01]);
+        root.left = reConstructBinaryTree(l1, head01 + 1, head01 + temp - head02,
+                l2, head02, temp - 1);
+        root.right = reConstructBinaryTree(l1, head01 + temp - head02 + 1, tail01,
+                l2, temp + 1, tail02);
+
+        return root;
     }
-//}
+
+    public static void main(String[] args) {
+        int[] a = new int[] {3, 9, 20, 15, 7};
+        int[] b = new int[] {9, 3, 15, 20, 7};
+        BinaryTreeNode node = reConstructBinaryTree(a, 0, 4, b, 0, 4);
+        System.out.println(node.value);
+        System.out.println(node.left.value);
+        System.out.println(node.right.value);
+        System.out.println(node.right.left.value);
+        System.out.println(node.right.right.value);
+    }
+}
+
+
