1462f312885f20d1a4f7a0fdb7046473e6949396
cgIIrw <chenguo_jay@163.com>
2018-09-27 21:02:15 +0800

    卡卡 数组中重复的数字

--- main/src/jianzhi_offer/Duplicate.java
+++ main/src/jianzhi_offer/Duplicate.java
@@ -1,4 +1,43 @@
 package jianzhi_offer;
 
+/**
+ * 卡卡 数组中重复的数字
+ */
 public class Duplicate {
+
+    public static boolean duplicate(int[] arr) {
+        if (arr == null) {
+            return false;
+        }
+
+        for (int e : arr) {
+            if (e < 0 || e > arr.length - 1) {
+                return false;
+            }
+        }
+
+        for (int i = 0; i < arr.length; i++) {
+            // 这一步if显得多余，但是是第一次编写的时候添加的，
+            // 这里不做更改交给以后review吧
+            if (i == arr[i]) {
+                continue;
+            } else {
+                while (i != arr[i]) {
+                    if (arr[i] == arr[arr[i]]) {
+                        return true;
+                    }
+                    int temp = arr[i];
+                    arr[i] = arr[arr[i]];
+                    arr[temp] = temp;
+                }
+            }
+        }
+       return false;
+    }
+
+    public static void main(String[] args) {
+
+        int[] arr = new int[] {17};
+        System.out.println(duplicate(arr));
+    }
 }
