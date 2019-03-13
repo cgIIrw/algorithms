92e1a664baaf59986eda6349b5e66b617f8fc022
cgIIrw <cgrwii@163.com>
2019-03-13 16:38:23 +0800

    添加RectCover

--- main/src/jz-offer/RectCover.java
+++ main/src/jz-offer/RectCover.java
@@ -1,4 +1,31 @@
-// 矩形覆盖
+// 矩形覆盖 继续是斐波拉契数列
 public class RectCover {
-  
+  public static int rectCover(int target) {
+    if (target < 0) {
+      throw new RuntimeException("参数出错！");
+    }
+
+    if (target == 0) {
+      return 1;
+    }
+
+    if (target == 1) {
+      return 1;
+    }
+
+    int a = 1;
+    int b = 1;
+
+    for (int i = 2; i<= target; i++) {
+      int temp = a + b;
+      a = b;
+      b = temp;
+    }
+    return b;
+  }
+
+  public static void main(String[] args) {
+    System.out.println(rectCover(5));
+
+  }
 }
